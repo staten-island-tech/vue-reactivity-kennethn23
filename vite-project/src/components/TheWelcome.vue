280a38f11b1845862a7b4767444076b6fa67507b
kennethn23 <kennethng1337@gmail.com>
2024-02-02 09:23:45 -0500

    2/2/24

---
+++ vite-project/src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="test === true">Good</h1>
+    <h1 v-else>Bad</h1>
+  </div>
+</template>
+
+<script setup>
+const test = false;
+</script>
+
+<style scoped>
+
+</style>
